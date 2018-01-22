dc490ad86f0180550027b7dd7dddbb3af15b71a7
Mikaela Suomalainen <mikaela+git@mikaela.info>
2018-01-22 21:08:13 +0200

    piraatit/irc: laziness hit me

---
+++ irc/piraatit/messi.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copied from https://stackoverflow.com/a/8880633 except the nicks
+
+## declare an array variable
+declare -a arr=("janneone" "mbrsk" "mielipuoli" "nyos" "usvi" "vhautaka" "yaamboo" "lash-" "d200b" "talikka" "arimartti" "scoffa")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "/msg GroupServ invite !ppfi-aktiivit $i"
+   # or do whatever with individual element of the array
+done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
