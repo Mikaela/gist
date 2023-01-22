73a1ca528f3c69d6efe4d10b7155f9d135392d8f
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-01-22 11:56:22 +0200

    vcard: initial commit

---
+++ vcard/generate-qr.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Turn vcards into QR codes
+
+
+
+# loop through all vcard files
+for vcard in *.vcard; do 
+    if [ -f "$vcard" ]; then 
+        # -l H - high error correction
+        # -t utf8 - output to terminal
+        # -r - read file
+        # -o output file
+        qrencode -l H -t utf8 -r $vcard
+        qrencode -l H -o $vcard.png -r $vcard
+    fi 
+done
