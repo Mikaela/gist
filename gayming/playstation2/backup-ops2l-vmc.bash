dedd4914b2601d9fefbe2d8768903e192cd6062c
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-03-16 17:27:11 +0200

    backup-ops2l-vmc.bash: initial commit

---
+++ gayming/playstation2/backup-ops2l-vmc.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This is just a simple script to backup VMCs in case something goes wrong
+set -x
+
+VMCDIR=~/PS2/VMC
+BACKUPDIR=~/PS2-VMC-backups
+
+mkdir -p $BACKUPDIR
+# E.g. 2023-075TT1719, I don't think change of month matters and these are
+# going to be small 8MB files anyway so seeing the age at glance is nice.
+# WARNING: This doesn't handle DST switch althůgh I don't expect to be
+# playing or running this script during the problem hours
+cp -rv "$VMCDIR" "$BACKUPDIR/$(date +%Y-%jT%H%M)"
+
+set +x
