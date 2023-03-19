7a84b50bece3b2272975e704f0918a358d5dc910
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-03-19 20:12:12 +0200

    gayming/playstation2/backup-ops2l-vmc.bash: add a to cp flags & fix typo

--- gayming/playstation2/backup-ops2l-vmc.bash
+++ gayming/playstation2/backup-ops2l-vmc.bash
@@ -8,9 +8,9 @@ BACKUPDIR=~/PS2-VMC-backups
 mkdir -p $BACKUPDIR
 # E.g. 2023-075TT1719, I don't think change of month matters and these are
 # going to be small 8MB files anyway so seeing the age at glance is nice.
-# WARNING: This doesn't handle DST switch althůgh I don't expect to be
+# WARNING: This doesn't handle DST switch although I don't expect to be
 # playing or running this script during the problem hours
-cp -rv "$VMCDIR" "$BACKUPDIR/$(date +%Y-%jT%H%M)"
+cp -arv "$VMCDIR" "$BACKUPDIR/$(date +%Y-%jT%H%M)"
 
 # Samba is fun with permissions
 chmod -R 777 $VMCDIR
