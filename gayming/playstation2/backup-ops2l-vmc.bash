a6f73dbad72d5958bec7e899bd27050f22b36a93
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-05-16 19:44:54 +0300

    backup-ops2l-vmc.bash: add seconds, summmarize du instead, add finishing date

--- gayming/playstation2/backup-ops2l-vmc.bash
+++ gayming/playstation2/backup-ops2l-vmc.bash
@@ -6,11 +6,11 @@ VMCDIR=~/PS2/VMC
 BACKUPDIR=~/PS2-VMC-backups
 
 mkdir -p $BACKUPDIR
-# E.g. 2023-075TT1719, I don't think change of month matters and these are
+# E.g. 2023-075TT171900, I don't think change of month matters and these are
 # going to be small 8MB files anyway so seeing the age at glance is nice.
 # WARNING: This doesn't handle DST switch although I don't expect to be
 # playing or running this script during the problem hours
-cp -arv "$VMCDIR" "$BACKUPDIR/$(date +%Y-%jT%H%M)"
+cp -arv "$VMCDIR" "$BACKUPDIR/$(date +%Y-%jT%H%M%S)"
 
 # Samba is fun with permissions
 chmod -R 777 $VMCDIR/..
@@ -24,6 +24,9 @@ if hash duperemove 2>/dev/null; then
 fi
 
 # Curiosity, show the space used
-du -hca --time $BACKUPDIR
+du -hcs --time $BACKUPDIR
+
+# When the script finished?
+date "+%Y-%jT%H%M%S"
 
 set +x
